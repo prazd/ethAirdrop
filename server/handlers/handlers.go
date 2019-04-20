01bcc8be32770f715b301b4c61d16b70b4b465a3
prazd <alexokda@list.ru>
2019-04-20 03:14:31 +0300

    fmt

--- server/handlers/handlers.go
+++ server/handlers/handlers.go
@@ -3,19 +3,19 @@ package handlers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/prazd/ethAirdrop/eth"
-	"net/http"
 	"log"
+	"net/http"
 )
 
-func Airdrop(c *gin.Context){
+func Airdrop(c *gin.Context) {
 	address := c.Param("address")
 
-	txHash,err := eth.SendEth(address)
-	if err != nil{
+	txHash, err := eth.SendEth(address)
+	if err != nil {
 		log.Println(err)
 		c.JSON(http.StatusInternalServerError, gin.H{"error": 500})
 		return
 	}
-	
+
 	c.JSON(http.StatusOK, gin.H{"txHash": txHash})
 }
